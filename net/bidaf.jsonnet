74e45035565ff2daca0724c5400f948a96d4a4a7
Meefly <admin@meijiex.vip>
2019-01-25 18:18:28 +0800

    dureader_reader

--- net/bidaf.jsonnet
+++ net/bidaf.jsonnet
@@ -1,22 +1,18 @@
  {
     "dataset_reader":{
-        "type":"msmarco_multi_passage_limited",
+        "type":"dureader_multi_passage_limited",
         "token_indexers":{
             "tokens":{
                 "type":"single_id",
                 "lowercase_tokens":true
             }
-            ,
-            "token_characters":{
-                "type":"characters"
-            }
+            // ,
+            // "token_characters":{
+            //     "type":"characters"
+            // }
         },
         "lazy": true,
-        "char_only": true,
-        "max_samples": 100,
-        "language": "zh",
-        "passage_length_limit": 500,
-        "question_length_limit": 50
+        "max_samples": 100
     },
     "train_data_path":"/data/nfsdata/meijie/data/dureader/preprocessed/trainset/train.json",
     // "validation_data_path":"/data/nfsdata/meijie/data/dureader/preprocessed/trainset/train.json",
@@ -31,22 +27,22 @@
                     "embedding_dim":300,
                     "trainable":true
                 }
-                ,
-                "token_characters":{
-                    "type":"glyph_encoder",
-                    "glyph_embsize": 128,
-                    "output_size": 128,
-                    "use_batch_norm": true,
-                    "encoder":{
-                        "type":"cnn",
-                        "embedding_dim":128,
-                        "num_filters":100,
-                        "ngram_filter_sizes":[
-                            1
-                        ]
-                    },
-                    "dropout":0.2
-                }
+                // ,
+                // "token_characters":{
+                //     "type":"glyph_encoder",
+                //     "glyph_embsize": 128,
+                //     "output_size": 128,
+                //     "use_batch_norm": true,
+                //     "encoder":{
+                //         "type":"cnn",
+                //         "embedding_dim":128,
+                //         "num_filters":100,
+                //         "ngram_filter_sizes":[
+                //             1
+                //         ]
+                //     },
+                //     "dropout":0.15
+                // }
             }
         },
         "highway_embedding_size":300,
@@ -58,7 +54,7 @@
             "input_size":300,
             "hidden_size":150,
             "num_layers":2,
-            "dropout":0.2
+            "dropout":0.15
         },
         "modeling_layer":{
             "type":"lstm",
@@ -66,7 +62,7 @@
             "input_size":1200,
             "hidden_size":150,
             "num_layers":2,
-            "dropout":0.2
+            "dropout":0.15
         },
         "match_layer":{
             "type":"lstm",
@@ -74,7 +70,7 @@
             "input_size":1500,
             "hidden_size":750,
             "num_layers":2,
-            "dropout":0.2
+            "dropout":0.15
         },
         "matrix_attention_layer": {
             "type": "linear",
@@ -87,7 +83,7 @@
             "input_size": 1500,
             "hidden_dim": 200,
             "lstm_layers": 2,
-            "dropout": 0.2
+            "dropout": 0.15
         },
         "span_end_lstm":{
             "type":"lstm",
@@ -95,13 +91,13 @@
             "input_size":2100,
             "hidden_size":300,
             "num_layers":2,
-            "dropout":0.2
+            "dropout":0.15
         },
         "ptr_dim":200,
         "max_num_passages": 5,
         "max_num_character": 4,
         "language": "zh",
-        "dropout":0.2
+        "dropout":0.15
     },
     "iterator":{
         "type":"bucket",
@@ -114,7 +110,7 @@
         "grad_norm":5,
         "patience":10,
         "validation_metric":"+rouge_L",
-        "cuda_device":2,
+        "cuda_device":1,
         "learning_rate_scheduler":{
             "type":"reduce_on_plateau",
             "factor":0.5,
---
+++ src/dureader_reader.py
@@ -0,0 +1,216 @@
+import json
+import logging
+import re
+import os.path
+import pickle
+import time
+from typing import Dict, List, Tuple, Optional, Iterable, Any
+from overrides import overrides
+from tqdm import tqdm as tqdm
+
+from allennlp.common.file_utils import cached_path
+from allennlp.data.dataset_readers.dataset_reader import DatasetReader
+from allennlp.data.instance import Instance
+from allennlp.data.dataset_readers.reading_comprehension import util
+from allennlp.data.token_indexers import SingleIdTokenIndexer, TokenIndexer
+from allennlp.data.tokenizers import Token, Tokenizer, WordTokenizer
+from allennlp.data.fields import Field, TextField, IndexField, \
+    MetadataField, LabelField, ListField, SequenceLabelField
+
+from .scripts.dataset import load_data
+from .scripts.rouge import Rouge
+from .utils import get_answers_with_RougeL
+from .scripts.addRouge_L import add_rouge_read
+
+logger = logging.getLogger(__name__)
+
+
+@DatasetReader.register("dureader_multi_passage_limited")
+class DuReaderMultiPassageReader(DatasetReader):
+    """
+    This class is loading multi-passage data.
+
+    Parameters
+    ----------
+    tokenizer : ``Tokenizer``, optional (default=``WordTokenizer()``)
+        We use this ``Tokenizer`` for both the question and the passage.  See :class:`Tokenizer`.
+        Default is ```WordTokenizer()``.
+    token_indexers : ``Dict[str, TokenIndexer]``, optional
+        We similarly use this for both the question and the passage.  See :class:`TokenIndexer`.
+        Default is ``{"tokens": SingleIdTokenIndexer()}``.
+    lazy : ``bool``, optional (default=False)
+        If this is true, ``instances()`` will return an object whose ``__iter__`` method
+        reloads the dataset each time it's called. Otherwise, ``instances()`` returns a list.
+    passage_length_limit : ``int``, optional (default=None)
+        if specified, we will cut the passage if the length of passage exceeds this limit.
+    question_length_limit : ``int``, optional (default=None)
+        if specified, we will cut the question if the length of passage exceeds this limit.
+    """
+
+    def __init__(self,
+                 embed_size: int = 300,
+                 max_p_num: int = 5,
+                 max_p_len: int = 500,
+                 max_q_len: int = 50,
+                 tokenizer: Tokenizer = None,
+                 token_indexers: Dict[str, TokenIndexer] = None,
+                 lazy: bool = False,
+                 max_samples: int = -1) -> None:
+        super().__init__(lazy)
+        self.max_p_num = max_p_num
+        self.max_p_len = max_p_len
+        self.max_q_len = max_q_len
+        self._tokenizer = tokenizer or WordTokenizer()
+        self._token_indexers = token_indexers or {'tokens': SingleIdTokenIndexer()}
+        self.max_samples = max_samples
+
+    @overrides
+    def _read(self, file_path: str) -> Iterable[Instance]:
+        if 'train' in file_path:
+            dataset = self._load_dataset(file_path, True)
+        else:
+            dataset = self._load_dataset(file_path)
+        for sample in dataset:
+            json_obj = self._dureader_sample_to_json_obj(sample)
+            yield self._json_blob_to_instance(json_obj)
+
+    @staticmethod
+    def segmented_text_to_tuples(tokens):
+        idx = 0
+        result = []
+        for text in tokens:
+            result.append((text, idx))
+            idx += len(text)
+        return result
+
+    def _dureader_sample_to_json_obj(slef, sample)->Dict:
+        '''
+        Input smaple
+        ------------
+        dict_keys(['documents', 'answer_spans', 'fake_answers', 'question', 'segmented_answers',
+        'answers', 'answer_docs', 'segmented_question', 'question_type', 'question_id',
+        'fact_or_opinion', 'match_scores', 'answer_passages', 'question_tokens', 'passages'])
+        '''
+        json_obj = {}
+        json_obj['question_tokens'] = self.segmented_text_to_tuples(sample['question_tokens'])
+        json_obj['passages_tokens'] = [segmented_text_to_tuples(sum(doc['passage_tokens'], []))
+                                       for doc in sample['passages']]
+        json_obj['passages_texts'] = [''.join(passage['passage_tokens']) for passage in sample['passages']]
+        json_obj['qid'] = sample.pop('question_id')
+        if 'answers' in sample:
+            json_obj['answer_texts'] = sample.pop('answers')
+        else:
+            json_obj['answer_texts'] = []
+        token_spans = [[(-1, -1)]] * len(json_obj['passages_texts'])
+        if 'answer_passages' in sample and len(sample['answer_passages']):
+            token_spans[sample['answer_docs'][0]] = sample['answer_spans'][0]
+        json_obj['token_spans'] = token_spans
+        return json_obj
+
+    def _json_blob_to_instance(self, json_obj) -> Instance:
+        question_tokens = [Token(text=text, idx=idx) for text, idx in json_obj['question_tokens']]
+        passages_tokens = [[Token(text=text, idx=idx) for text, idx in passage_tokens]
+                           for passage_tokens in json_obj['passages_tokens']]
+        passages_texts = json_obj['passages_texts']
+        qid = json_obj['qid']
+        answer_texts = json_obj['answer_texts']
+        token_spans = json_obj['token_spans']
+        return self.make_MSMARCO_MultiPassage_instance(question_tokens,
+                                                       passages_tokens,
+                                                       self._token_indexers,
+                                                       passages_texts,
+                                                       qid,
+                                                       answer_texts,
+                                                       token_spans)
+
+    def _load_dataset(self, data_path, train=False):
+        """
+        Loads the dataset
+        Args:
+            data_path: the data file to load
+        """
+        with open(data_path) as fin:
+            data_set = []
+            for lidx, line in tqdm(enumerate(fin)):
+                if self.max_samples >= 0 and lidx > self.max_samples:
+                    break
+                sample = json.loads(line.strip())
+                if train:
+                    if len(sample['answer_spans']) == 0:
+                        continue
+                    if sample['answer_spans'][0][1] >= self.max_p_len:
+                        continue
+
+                if 'answer_docs' in sample:
+                    sample['answer_passages'] = sample['answer_docs']
+
+                sample['question_tokens'] = sample['segmented_question']
+
+                sample['passages'] = []
+                for d_idx, doc in enumerate(sample['documents']):
+                    if train:
+                        most_related_para = doc['most_related_para']
+                        sample['passages'].append(
+                            {'passage_tokens': doc['segmented_paragraphs'][most_related_para],
+                             'is_selected': doc['is_selected']}
+                        )
+                    else:
+                        para_infos = []
+                        for para_tokens in doc['segmented_paragraphs']:
+                            question_tokens = sample['segmented_question']
+                            common_with_question = Counter(para_tokens) & Counter(question_tokens)
+                            correct_preds = sum(common_with_question.values())
+                            if correct_preds == 0:
+                                recall_wrt_question = 0
+                            else:
+                                recall_wrt_question = float(correct_preds) / len(question_tokens)
+                            para_infos.append((para_tokens, recall_wrt_question, len(para_tokens)))
+                        para_infos.sort(key=lambda x: (-x[1], x[2]))
+                        fake_passage_tokens = []
+                        for para_info in para_infos[:1]:
+                            fake_passage_tokens += para_info[0]
+                        sample['passages'].append({'passage_tokens': fake_passage_tokens})
+                data_set.append(sample)
+        return data_set
+
+    def make_MSMARCO_MultiPassage_instance(self,
+                                           question_tokens: List[Token],
+                                           passages_tokens: List[List[Token]],
+                                           token_indexers: Dict[str, TokenIndexer],
+                                           passages_texts: List[str],
+                                           qid: int,
+                                           answer_texts: List[str] = None,
+                                           token_spans: List[List[Tuple[int, int]]] = None,
+                                           additional_metadata: Dict[str, Any] = None) -> Instance:
+
+        fields: Dict[str, Field] = {}
+        additional_metadata = additional_metadata or {}
+        passages_offsets = [[(token.idx, token.idx + len(token.text)) for token in passage_tokens]
+                            for passage_tokens in passages_tokens]
+
+        fields['question'] = TextField(question_tokens, token_indexers)
+        passages_field = [TextField(p_tokens, token_indexers) for p_tokens in passages_tokens]
+        fields['passages'] = ListField(passages_field)
+        metadata = {'original_passages': passages_texts,
+                    'passages_offsets': passages_offsets,
+                    'qid': qid,
+                    'question_tokens': [token.text for token in question_tokens],
+                    'passage_tokens': [[token.text for token in passage_tokens]
+                                       for passage_tokens in passages_tokens]}
+        if answer_texts:
+            metadata['answer_texts'] = answer_texts
+        if token_spans is None or not token_spans:
+            token_spans = [[(-1, -1)]] * len(passages_texts)
+        spans_start = []
+        spans_end = []
+        for (idx, spans_in_passage), passage_field in zip(enumerate(token_spans), passages_field):
+            spans_start.append(ListField([IndexField(span_start, passage_field)
+                                          for span_start, span_end in spans_in_passage]))
+            spans_end.append(ListField([IndexField(span_end, passage_field)
+                                        for span_start, span_end in spans_in_passage]))
+        fields['spans_start'] = ListField(spans_start)
+        fields['spans_end'] = ListField(spans_end)
+
+        metadata.update(additional_metadata)
+        fields['metadata'] = MetadataField(metadata)
+        return Instance(fields)
